9951acaed95e8d27534134cd8a7bbe44ae152eb8
Alison Williams <apw5011@gmail.com>
2020-11-18 21:02:38 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -8,12 +8,8 @@ class Shoe
     @brand = brand 
   end 
   
-  def cobble=(cobble)
-    @cobble = cobble 
-  end 
-  
   def cobble 
-    @cobble
+    puts "Your shoe is as good as new!"
   end 
   
 end 
